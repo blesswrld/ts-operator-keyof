a278294707e6e16fda1bcea8e00c5e4abfff607a
blesswrld <roninfortnite71@gmail.com>
2025-05-08 22:43:59 +0300

    operator keyof example

---
+++ index.ts
@@ -0,0 +1,34 @@
+// Интерфейс
+interface ICompany {
+    name: string;
+    debts: number;
+}
+
+// Тип с ключами
+type CompanyKeys = keyof ICompany;
+const keys: CompanyKeys = "name"; // Берем ключ name из интерфейса компании
+
+// Generic функция
+function printDebts<T, K extends keyof T, S extends keyof T>(
+    company: T,
+    name: K,
+    debts: S
+) {
+    console.log(`Company ${company[name]}, debts: ${company[debts]}`);
+}
+
+const hh: ICompany = {
+    name: "HH",
+    debts: 500000,
+};
+
+// Вызов функции
+printDebts(hh, "name", "debts");
+
+const google = {
+    name: "Google",
+    open: true,
+};
+
+// Вызов функции
+printDebts(google, "name", "open");
