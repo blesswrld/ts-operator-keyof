583806b63edc37b95571c1f5e057597b30309252
blesswrld <roninfortnite71@gmail.com>
2025-05-08 23:08:30 +0300

    indexed access types

--- index.ts
+++ index.ts
@@ -1,8 +1,35 @@
-// Интерфейс
+// Интерфейсы
 interface ICompany {
     name: string;
     debts: number;
+    departments: Department[];
+    managment: {
+        owner: string;
+    };
 }
+
+interface Department {
+    // Фиксируем значение ключей
+    [key: string]: string;
+}
+
+// const debts = "debts";
+// Убираем ошибку для глобальной переменной с помощью назначение литерального типа
+// let debts = "debts" as "debts";
+// Либо с помощью аннотации типа
+let debts: "debts" = "debts";
+// Делаем запрос через typeof к debts
+type CompanyDebtsType = ICompany[typeof debts];
+
+// Indexed access type (доступ по индексному ключу)
+// type CompanyDebtsType = ICompany["debts"];
+// Получаем тип владельца (вложенный объект)
+type CompanyOwnerType = ICompany["managment"]["owner"];
+//
+type CompanyDepartmentsType = ICompany["departments"][number];
+type CompanyDepartmentsTypes = ICompany["departments"];
+// Получаем Union-type со всеми свойствами объекта
+type Test = ICompany[keyof ICompany];
 
 // Тип с ключами
 type CompanyKeys = keyof ICompany;
@@ -17,21 +44,21 @@ function printDebts<T, K extends keyof T, S extends keyof T>(
     console.log(`Company ${company[name]}, debts: ${company[debts]}`);
 }
 
-const hh: ICompany = {
-    name: "HH",
-    debts: 500000,
-};
-
-// Вызов функции
-printDebts(hh, "name", "debts");
-
-const google = {
+const google: ICompany = {
     name: "Google",
-    open: true,
+    debts: 500000,
+    departments: {
+        sales: "sales",
+        developer: "dev",
+    },
+
+    managment: {
+        owner: "John",
+    },
 };
 
 // Вызов функции
-printDebts(google, "name", "open");
+printDebts(google, "name", "debts");
 
 // Делаем запрос типа по ключам
 type GoogleKeys = keyof typeof google;
